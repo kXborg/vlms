3af319a9a54c43a402fea780394dc3da0b7575bf
kXborg <kukilp213@gmail.com>
2025-12-25 12:05:57 +0530

    work

--- Experiments/chatApp/new.py
+++ Experiments/chatApp/new.py
@@ -1,43 +1,53 @@
+from openai import OpenAI
 import gradio as gr
 
+MAX_TURNS = 6
+def build_prompt(system_msg, user_msg, assistant_msg):
+	global chat_history
 
-def get_max_tokens() -> int:
-	return 1024
+	if not chat_history:
+        chat_history.append({
+            "role": "system",
+            "content": system_msg
+        })
+    else:
+        chat_history[0] = {
+            "role": "system",
+            "content": system_msg
+        }
+        
+	chat_history.append({"role":"user", "content":user_msg})
+	chat_history.append({"role":"assistant","content":assistant_msg})
 
-def on_send(usr_msg, stats_palceholder):
-	usr_msg = usr_msg or "".strip()
+	system_entry = chat_history[0]
+    recent = chat_history[-(MAX_TURNS * 2):]
 
-	try:
-		resp = chat_once(usr_msg, get_max_tokens())
-	except BadRequestError:
-		yield "Context Limit Reached, please start a new chat or shorten inputs"
+    chat_history[:] = [system_entry] + recent
 
-	answer = resp.choices[0].message.content or ""
-	yield answer
-
-
-def build_ui():
-	"""Build Gradio User Interface"""
-	with gr.Blocks(title="Chat with Your Model") as demo:
-		gr.Markdown("## Simple Chat UI")
-
-		chatbot = gr.Chatbot(height=350)
-
-		with gr.Row():
-			msg = gr.Textbox(placeholder="Type your message ...", scale=4)
-			send_btn = gr.Button("Send", variant="primary")
-
-		# Events
-		send_event = send_btn.click(
-			fn = on_send,
-			inputs = [msg],
-			outputs = [chatbot, msg])
-
-	return demo
+    return chat_history
 
 
-if __name__ == "__main__":
-	ui = build_ui()
-	host = "127.0.0.1"
-	port = "7860"
-	ui.queue.launch(host, port)
+def get_system_prompt(input):
+	"""Define later"""
+	return input
+
+def build_query(system_prompt, input_msg, history) -> str:
+	message = system_prompt + input_msg + ". Earlier context: " + history
+	return message
+
+def get_response():
+	client = OpenAI(
+		base_url = "https://deplorably-athonite-loreta.ngrok-free.dev/v1",
+		api_key = "dummy"
+		)
+
+	response = client.chat.completions.create(
+		model = "Qwen/Qwen3-0.6B",
+		messages=[{"role": "user", "content": build_query()}],
+		stream=True,
+		)
+
+	for chunk in response:
+		delta = chunk.choices[0].delta
+		if delta and delta.content:
+			print(delta.content, end="", flush=True)
